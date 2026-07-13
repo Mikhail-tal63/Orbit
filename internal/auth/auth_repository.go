a5f6484c170bf228de806a74000167fa8a3c606e
Jehad Mohamed <jjehad100@gmail.com>
2026-07-13 17:42:57 +0200

    feat: add sqlc to project : auth_repository.go , db/querys , sqlc.yaml

--- internal/auth/auth_repository.go
+++ internal/auth/auth_repository.go
@@ -3,60 +3,24 @@ package auth
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/Mikhail-Tal63/Orbit/internal/db"
 )
 
+
+
 type AuthRepository struct {
-	db *pgxpool.Pool
+	queries *db.Queries
 }
 
-func NewAuthRepository(db *pgxpool.Pool) *AuthRepository {
+func NewAuthRepository(q *db.Queries) *AuthRepository {
 	return &AuthRepository{
-		db: db,
+		queries: q,
 	}
 }
+func (r *AuthRepository) CreateUser(
+	ctx context.Context,
+	params db.CreateUserParams,
+) error {
 
-func (r *AuthRepository) CreateUser(ctx context.Context, user *User) error {
-	query := `
-		INSERT INTO users (
-			id,
-			first_name,
-			last_name,
-			email,
-			password_hash,
-			phone,
-			role,
-			image_id,
-			is_active,
-			created_at,
-			updated_at,
-			last_login_at
-		)
-		VALUES (
-			$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12
-		)
-	`
-
-	_, err := r.db.Exec(
-		ctx,
-		query,
-		user.ID,
-		user.FirstName,
-		user.LastName,
-		user.Email,
-		user.PasswordHash,
-		user.Phone,
-		user.Role,
-		user.ImageID,
-		user.IsActive,
-		user.CreatedAt,
-		user.UpdatedAt,
-		user.LastLoginAt,
-	)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return r.queries.CreateUser(ctx, params)
 }
---
+++ internal/db/querys/users.sql
@@ -0,0 +1,42 @@
+-- name: CreateUser :exec
+INSERT INTO users (
+    id,
+    first_name,
+    last_name,
+    email,
+    password_hash,
+    phone,
+    role,
+    image_id,
+    is_active,
+    created_at,
+    updated_at,
+    last_login_at
+)
+VALUES (
+    $1, $2, $3, $4, $5, $6,
+    $7, $8, $9, $10, $11, $12
+);
+
+-- name: GetUserByEmail :one
+SELECT
+    id,
+    first_name,
+    last_name,
+    email,
+    password_hash,
+    phone,
+    role,
+    image_id,
+    is_active,
+    created_at,
+    updated_at,
+    last_login_at
+FROM users
+WHERE email = $1
+LIMIT 1;
+
+-- name: UpdateLastLogin :exec
+UPDATE users
+SET last_login_at = $2
+WHERE id = $1;
