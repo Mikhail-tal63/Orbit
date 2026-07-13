1558bdf2e09e6e689a5a71c66af87b2eee690633
Jehad Mohamed <jjehad100@gmail.com>
2026-07-13 18:01:44 +0200

    feat: add get user by id query : users.sql

--- internal/db/querier.go
+++ internal/db/querier.go
@@ -6,11 +6,14 @@ package db
 
 import (
 	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Querier interface {
 	CreateUser(ctx context.Context, arg CreateUserParams) error
 	GetUserByEmail(ctx context.Context, email string) (User, error)
+	GetUserByID(ctx context.Context, id pgtype.UUID) (User, error)
 	UpdateLastLogin(ctx context.Context, arg UpdateLastLoginParams) error
 }
 
