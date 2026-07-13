0145b66f3fef7ce32cf43b4860099bc733b96359
Jehad Mohamed <jjehad100@gmail.com>
2026-07-13 11:39:31 +0200

    feat: add db connect to main : api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Mikhail-Tal63/Orbit/configs"
+	"github.com/Mikhail-Tal63/Orbit/internal/database"
+)
 
 func main() {
+
+	cfg := configs.Load()
+
+	db := database.Connect(*cfg)
+	defer db.Close()
+
 	fmt.Println("Orbit API starting...")
 }
