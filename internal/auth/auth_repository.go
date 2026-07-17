53efbf25c2bd34b1db52d2f843ea910e71fdb858
Jehad Mohamed <jjehad100@gmail.com>
2026-07-17 20:47:04 +0200

    fix: changing repository for testing: repository

--- internal/auth/auth_repository.go
+++ internal/auth/auth_repository.go
@@ -5,35 +5,72 @@ import (
 
 	"github.com/Mikhail-Tal63/Orbit/internal/db"
 	"github.com/google/uuid"
-
 )
 
-type AuthRepository struct {
+type AuthRepository interface {
+
+	CreateUser(
+		ctx context.Context,
+		params db.CreateUserParams,
+	) (*db.User, error)
+
+
+	GetUserByEmail(
+		ctx context.Context,
+		email string,
+	) (*db.User, error)
+
+
+	GetUserByUsername(
+		ctx context.Context,
+		username string,
+	) (*db.User, error)
+
+
+	GetUserByID(
+		ctx context.Context,
+		id uuid.UUID,
+	) (*db.User, error)
+}
+
+
+type AuthRepositoryImpl struct {
 	queries *db.Queries
 }
 
-func NewAuthRepository(q *db.Queries) *AuthRepository {
-	return &AuthRepository{
+var _ AuthRepository = (*AuthRepositoryImpl)(nil)
+
+
+
+func NewAuthRepository(q *db.Queries) *AuthRepositoryImpl {
+
+	return &AuthRepositoryImpl{
 		queries: q,
 	}
 }
-func (r *AuthRepository) CreateUser(
+
+
+func (r *AuthRepositoryImpl) CreateUser(
 	ctx context.Context,
 	params db.CreateUserParams,
-) (*db.User,error) {
+) (*db.User, error) {
 
-	 user,err := r.queries.CreateUser(ctx, params)
-	 if err != nil {
-		return nil,err
-	 }
-	 return &user,err
+	user, err := r.queries.CreateUser(ctx, params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
-func (r *AuthRepository) GetUserByEmail(
+
+func (r *AuthRepositoryImpl) GetUserByEmail(
 	ctx context.Context,
 	email string,
 ) (*db.User, error) {
 
 	user, err := r.queries.GetUserByEmail(ctx, email)
+
 	if err != nil {
 		return nil, err
 	}
@@ -41,19 +78,34 @@ func (r *AuthRepository) GetUserByEmail(
 	return &user, nil
 }
 
-func (r *AuthRepository) GetUserByID(ctx context.Context, id uuid.UUID) (*db.User, error) {
-	user, err := r.queries.GetUserByID(ctx, id)
-	if err != nil {
-		return nil, err
-	}
 
-	return &user, nil
-}
 
-func (r *AuthRepository) GetUserByUsername(ctx context.Context, username string) (*db.User, error) {
+func (r *AuthRepositoryImpl) GetUserByUsername(
+	ctx context.Context,
+	username string,
+) (*db.User, error) {
+
 	user, err := r.queries.GetUserByUsername(ctx, username)
+
 	if err != nil {
 		return nil, err
 	}
+
+	return &user, nil
+}
+
+
+
+func (r *AuthRepositoryImpl) GetUserByID(
+	ctx context.Context,
+	id uuid.UUID,
+) (*db.User, error) {
+
+	user, err := r.queries.GetUserByID(ctx, id)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &user, nil
 }
