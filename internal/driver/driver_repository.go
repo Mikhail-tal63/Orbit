06f48545be15ee1d8b3158a44fd093788a3db7b0
Jehad Mohamed <jjehad100@gmail.com>
2026-07-31 14:09:33 +0200

    refactor: use interfaces for dependency injection

--- internal/driver/driver_repository.go
+++ internal/driver/driver_repository.go
@@ -6,17 +6,23 @@ import (
 	"github.com/Mikhail-Tal63/Orbit/internal/db"
 )
 
-type DriverRepository struct {
+type DriverRepository interface{
+	CreateDriver(ctx context.Context,params db.CreateDriverParams)(*db.Driver,error)
+}
+
+type DriverRepositoryImpl struct {
 	queries *db.Queries
 }
 
-func NewDriverRepository(db *db.Queries)*DriverRepository{
-	return &DriverRepository{
+
+
+func NewDriverRepository(db *db.Queries)*DriverRepositoryImpl{
+	return &DriverRepositoryImpl{
 		queries:db,
 	}
 }
 
-func (r *DriverRepository) CreateDriver(ctx context.Context,params db.CreateDriverParams)(*db.Driver,error){
+func (r *DriverRepositoryImpl) CreateDriver(ctx context.Context,params db.CreateDriverParams)(*db.Driver,error){
 	driver,err := r.queries.CreateDriver(ctx,params);
 	if err!= nil {
 return nil,err
