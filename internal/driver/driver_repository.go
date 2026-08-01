224482e4b55b19eb7ac0b6c32a4914c946b98368
Jehad Mohamed <jjehad100@gmail.com>
2026-08-01 19:09:40 +0200

    refactor : saprate repository and repository implmntation for future testing : repository

--- internal/driver/driver_repository.go
+++ internal/driver/driver_repository.go
@@ -4,28 +4,35 @@ import (
 	"context"
 
 	"github.com/Mikhail-Tal63/Orbit/internal/db"
+	"github.com/google/uuid"
 )
 
-type DriverRepository interface{
-	CreateDriver(ctx context.Context,params db.CreateDriverParams)(*db.Driver,error)
+type DriverRepository interface {
+	CreateDriver(ctx context.Context, params db.CreateDriverParams) (*db.Driver, error)
 }
 
 type DriverRepositoryImpl struct {
 	queries *db.Queries
 }
 
-
-
-func NewDriverRepository(db *db.Queries)*DriverRepositoryImpl{
+func NewDriverRepository(db *db.Queries) *DriverRepositoryImpl {
 	return &DriverRepositoryImpl{
-		queries:db,
+		queries: db,
 	}
 }
 
-func (r *DriverRepositoryImpl) CreateDriver(ctx context.Context,params db.CreateDriverParams)(*db.Driver,error){
-	driver,err := r.queries.CreateDriver(ctx,params);
-	if err!= nil {
-return nil,err
+func (r *DriverRepositoryImpl) CreateDriver(ctx context.Context, params db.CreateDriverParams) (*db.Driver, error) {
+	driver, err := r.queries.CreateDriver(ctx, params)
+	if err != nil {
+		return nil, err
 	}
-	return &driver,nil
+	return &driver, nil
+}
+
+func (r *DriverRepositoryImpl) GetDriverByUserId(ctx context.Context, userID uuid.UUID) (*db.Driver, error) {
+	driver, err := r.queries.GetDriverByUserId(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	return &driver, nil
 }
