2c21949e4eaf113bec50a1232c91d17622843d40
Jehad Mohamed <jjehad100@gmail.com>
2026-08-14 23:51:24 +0200

    feat : add write pump and red pump methods : websocket

---
+++ internal/websocket/handler.go
@@ -0,0 +1,103 @@
+package websocket
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Mikhail-Tal63/Orbit/middleware"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait      = 10 * time.Second
+	pongwait       = 10 * time.Second
+	pingoeriod     = (pongwait * 9) / 10
+	maxMessageSize = 1 << 16
+)
+
+var upgrade = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		origin := r.Header.Get("Origin")
+		if origin == "" {
+			return true
+		}
+		return middleware.IsAllowedOrigin(origin)
+	},
+}
+
+func (c *Client) ReadPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+
+	c.conn.SetReadLimit(maxMessageSize)
+	_ = c.conn.SetReadDeadline(time.Now().Add(pongwait))
+	c.conn.SetPongHandler(func(string) error {
+		return c.conn.SetReadDeadline(time.Now().Add(pongwait))
+	})
+
+	for {
+		_, row, err := c.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		_ = c.conn.SetReadDeadline(time.Now().Add(pongwait))
+
+		var env struct {
+			Type   string `json:"type"`
+			RideID string `json:"ride_id"`
+		}
+
+		if err := json.Unmarshal(row, &env); err != nil {
+			continue
+		}
+		switch env.Type {
+		case "join_ride":
+			c.hub.AddClient(c)
+		case "leave_ride":
+			c.hub.RemoveClient(c)
+		case "ping":
+			pong, _ := json.Marshal(map[string]string{"Type": "pong"})
+			select {
+			case c.send <- pong:
+			default:
+			}
+
+		}
+
+	}
+}
+
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingoeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.send:
+			_ = c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				_ = c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+				log.Printf("ws: write error user=%s: %v", c.userID, err)
+				return
+			}
+		case <-ticker.C:
+			_ = c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PongMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
