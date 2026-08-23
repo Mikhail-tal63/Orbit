a507667416e4555eadce7bb1645c91d396b66d30
Jehad Mohamed <jjehad100@gmail.com>
2026-08-23 18:27:58 +0200

    feat : add find nearby driver methods : location

--- internal/location/location_store.go
+++ internal/location/location_store.go
@@ -7,23 +7,22 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-
 const driverLocationKey = "drivers:locations"
 
 type RedisLocationStore struct {
 	client *redis.Client
 }
 
-func NewRedisLocationStore(client *redis.Client)*RedisLocationStore{
+func NewRedisLocationStore(client *redis.Client) *RedisLocationStore {
 	return &RedisLocationStore{
 		client: client,
 	}
 }
 
-func (r *RedisLocationStore) UpdateDriverLocation(ctx context.Context,driverID string, 	latitude float64,longitude float64,)error{
-	err := r.client.GeoAdd(ctx,driverID,&redis.GeoLocation{
+func (r *RedisLocationStore) UpdateDriverLocation(ctx context.Context, driverID string, latitude float64, longitude float64) error {
+	err := r.client.GeoAdd(ctx, driverID, &redis.GeoLocation{
 		Longitude: longitude,
-		Latitude: latitude,
+		Latitude:  latitude,
 	}).Err()
 	if err != nil {
 		return fmt.Errorf("failed to update driver location: %w", err)
@@ -32,11 +31,27 @@ func (r *RedisLocationStore) UpdateDriverLocation(ctx context.Context,driverID s
 	return nil
 }
 
-func (r *RedisLocationStore) RemoveDriverLocation(ctx context.Context,driverID string)error{
-	err := r.client.ZRem(ctx,driverLocationKey,driverID).Err()
-if err != nil{
-	return fmt.Errorf("failed to remove driver location: %w", err)
-}
- return nil
+func (r *RedisLocationStore) RemoveNearbyLocation(ctx context.Context, driverID string) error {
+	err := r.client.ZRem(ctx, driverLocationKey, driverID).Err()
+	if err != nil {
+		return fmt.Errorf("failed to remove driver location: %w", err)
+	}
+	return nil
 }
 
+func (r *RedisLocationStore) FindDriverLocation(ctx context.Context, latitude float64, longitude float64, radiuskm float64) ([]string, error) {
+	drivers, err := r.client.GeoSearch(ctx, driverLocationKey, &redis.GeoSearchQuery{
+		Latitude:   latitude,
+		Longitude:  longitude,
+		Radius:     radiuskm,
+		RadiusUnit: "km",
+		Sort:       "ASC",
+	},
+	).Result()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to find nearby drivers: %w", err)
+	}
+	return drivers, nil
+
+}
