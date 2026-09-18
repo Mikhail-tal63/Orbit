5948cab10bbd80865b2bccab8c8a4d1d61f393cb
Jehad Mohamed <jjehad100@gmail.com>
2026-09-18 20:17:59 +0200

    feat :add 200~GetRoute method:geoapify

--- internal/geocoding/geoapify.go
+++ internal/geocoding/geoapify.go
@@ -2,6 +2,7 @@ package geocoding
 
 import (
 	"context"
+	
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -12,7 +13,10 @@ type GeoapifyService struct {
 	apiKey string
 	client *http.Client
 }
-
+type Route struct{
+	DistanceKm  float64
+    DurationMin int
+}
 func NewGeoapifyService(apiKey string) *GeoapifyService {
 	return &GeoapifyService{
 		apiKey: apiKey,
@@ -66,3 +70,56 @@ func (s *GeoapifyService) ReverseGeocode(ctx context.Context, lat, lon float64) 
 
 	return result.Results[0].Formated, nil
 }
+
+func (s *GeoapifyService) GetRoute(ctx context.Context,
+	pickupLat ,pickupLon float64,
+	 destinationLat, destinationLon float64)(Route,error){
+	    url := fmt.Sprintf(
+        "https://api.geoapify.com/v1/routing?waypoints=%f,%f|%f,%f&mode=drive&apiKey=%s",
+        pickupLat,
+        pickupLon,
+        destinationLat,
+        destinationLon,
+        s.apiKey,
+
+    )
+req ,err := http.NewRequestWithContext(ctx,http.MethodGet,url,nil)
+if err!= nil {
+	return Route{},err
+}
+resp,err := s.client.Do(req)
+if err != nil {
+	return Route{},err
+}
+defer req.Body.Close()
+
+if resp.StatusCode != http.StatusOK{
+	    return Route{}, fmt.Errorf(
+            "geoapify routing returned status %d",
+            resp.StatusCode,
+        )
+}
+    var result struct {
+        Features []struct {
+            Properties struct {
+                Distance float64 `json:"distance"`
+                Time     float64 `json:"time"`
+            } `json:"properties"`
+        } `json:"features"`
+    }
+	   if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+        return Route{}, err
+    }
+
+    if len(result.Features) == 0 {
+        return Route{}, fmt.Errorf("no route found")
+    }
+
+    distanceMeters := result.Features[0].Properties.Distance
+    durationSeconds := result.Features[0].Properties.Time
+
+    return Route{
+        DistanceKm:  distanceMeters / 1000,
+        DurationMin: int(durationSeconds / 60),
+    }, nil
+	 }
